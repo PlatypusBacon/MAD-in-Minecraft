913d1b0e8b7991c6409e36857f5eaddfaee799b0
Tyler <bergmantyler0@gmail.com>
2026-04-17 12:11:12 +1000

    officially doing it over minecraft paintings

--- src/main/java/bunger/group/MutuallyAssuredDestruction.java
+++ src/main/java/bunger/group/MutuallyAssuredDestruction.java
@@ -20,7 +20,6 @@ public class MutuallyAssuredDestruction implements ModInitializer {
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
