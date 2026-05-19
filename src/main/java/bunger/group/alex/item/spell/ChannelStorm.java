041f2ed020b94f74e18dee1240d801f86212721e
Alex <alexwall.ace312@gmail.com>
2026-05-19 22:48:08 +1000

    Spell refactoring

--- src/main/java/bunger/group/alex/item/spell/ChannelStorm.java
+++ src/main/java/bunger/group/alex/item/spell/ChannelStorm.java
@@ -3,8 +3,10 @@ package bunger.group.alex.item.spell;
 import bunger.group.alex.ParticleHelpers;
 import bunger.group.alex.spell.SpellHelpers;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LightningBolt;
@@ -12,8 +14,10 @@ import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.ClipContext;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.saveddata.WeatherData;
 import net.minecraft.world.phys.*;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.Optional;
 
@@ -22,56 +26,35 @@ public class ChannelStorm extends SpellTemplate {
         super(properties, 80, 40, SpellTypes.LIGHTNING);
     }
 
+
     @Override
     public void cast(Level level, LivingEntity user, ItemStack stack) {
         if (level.isClientSide()) {
-            return; // I lowkey dont fuck with client only magic
+            return;
         }
-        double range = (double) this.RANGE;
+
         Vec3 start = user.getEyePosition();
-        Vec3 look = user.getLookAngle();
-        Vec3 end = start.add(look.scale(range));
-
-        BlockHitResult blockHit = level.clip(new ClipContext(
-                start,
-                end,
-                ClipContext.Block.OUTLINE,
-                ClipContext.Fluid.NONE,
-                user
-        ));
-
-
-        EntityHitResult entityHit = null;
-        double closestDist = range * range;
-
-        AABB searchBox = user.getBoundingBox()
-                .expandTowards(look.scale(range))
-                .inflate(1.0);
-
-        for (Entity entity : level.getEntities(user, searchBox, e -> e instanceof LivingEntity && e != user)) {
-
-            AABB aabb = entity.getBoundingBox().inflate(0.3);
-
-            Optional<Vec3> hitPoint = aabb.clip(start, end);
-
-            if (hitPoint.isPresent()) {
-                double dist = start.distanceToSqr(hitPoint.get());
-
-                if (dist < closestDist) {
-                    closestDist = dist;
-                    entityHit = new EntityHitResult(entity, hitPoint.get());
-                }
-            }
+        Vec3 target = getCastTarget(user);
+        Vec3 dir;
+        if (target != null) {
+            dir = target.subtract(start).normalize();
+        } else {
+            dir = user.getLookAngle();
         }
 
+        advanceBeam(level, user, dir, start, 1, 1, 1, ParticleTypes.CLOUD);
+    }
+
+    @Override
+    public void impactResult(Level level, LivingEntity user, @Nullable BlockHitResult blockHit, @Nullable LivingEntity entityHit)
+    {
         Vec3 centre = null;
-        if (blockHit.getType() != HitResult.Type.MISS) {
+        if (blockHit != null) {
             BlockPos pos = blockHit.getBlockPos();
             centre = Vec3.atCenterOf(pos).add(0, 0.6, 0);
         }
         if (entityHit != null) {
-            Entity entity = entityHit.getEntity();
-            centre = entity.position().add(0, 0.1, 0);
+            centre = entityHit.position().add(0, 0.1, 0);
         }
 
         if (centre != null) {
@@ -121,7 +104,5 @@ public class ChannelStorm extends SpellTemplate {
                 }
             });
         }
-
-        ParticleHelpers.spawnBeamParticles(level, start, end, ParticleTypes.CLOUD);
     }
 }
